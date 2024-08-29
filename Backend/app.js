4dc810100767f4ba40a9645b749fa95eb1130246
juhid30 <55.juhideore@gmail.com>
2024-08-30 01:33:05 +0530

    Tournaments

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,15 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
 
-const app = express();
+const app = express()
 
 //setting cors
 //also read other options from docs
-app.use(
-  cors({
+app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+    credentials: true
+}))
 
 //setting a limit to accept JSON 
 app.use(express.json({
@@ -19,52 +17,26 @@ app.use(express.json({
 }))
 
 //setting up URL encoder
-app.use(
-  express.urlencoded({
+app.use(express.urlencoded({
     extended: true,
     //extended allows nested objects from URL (idk what it is)
-    limit: "16kb"
+    limit: "16000000000000000kb"
 }))
 
-//public assets which we have used (public folder)
-app.use(express.static("public"));
 
-//getting cookies and accessing/setting them
-app.use(cookieParser());
+
+//public assets which we have used (public folder)
+app.use(express.static("public"))
+
+//getting cookies and accessing/setting them 
+app.use(cookieParser())
+
 
 //routes import
-import userRouter from "./routes/UserRoute.js";
-import axios from "axios";
+import userRouter from "./routes/UserRoute.js"
 
 //routes declaration
-app.use("/api/v1/users", userRouter);
+app.use("/api/v1/users" , userRouter)
 
-app.post("/tournaments", async (req, res) => {
-  try {
-    const { query } = req.body;
-    const apiKey =
-      "faf77a60da15f38033845500f580e07a5056996fe55844af874cbd095e545f43";
 
-    if (!apiKey) {
-      throw new Error("API key is missing");
-    }
-
-    console.log("API Key:", apiKey); // For debugging purposes
-
-    const response = await axios.get(`https://serpapi.com/search.json`, {
-      params: {
-        q: `${query} online tournaments`,
-        num: 15,
-        api_key:
-          "faf77a60da15f38033845500f580e07a5056996fe55844af874cbd095e545f43",
-      },
-    });
-
-    res.json(response.data);
-  } catch (error) {
-    console.error("Error fetching tournaments:", error.message);
-    res.status(500).json({ message: "Error fetching tournaments" });
-  }
-});
-
-export { app };
+export {app}
