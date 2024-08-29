598ed9b54169557039fcbe9a0a7d796e2bd0b802
Atharva <atharvamainwork@gmail.com>
2024-08-29 12:46:06 +0530

    routes added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage';
+import { Games } from './Pages/Games';
+import CommunityForum from './Pages/Community';
+import { Tournaments } from './Pages/Tournaments';
+import { Merch } from './Pages/Merch';
+import { ProfilePage } from './Pages/ProfilePage';
 
 function App() {
   return (
@@ -8,6 +13,11 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/community" element={<LandingPage />} />
+        <Route path="/games" element={<Games></Games>} />
+        <Route path="/community" element={<CommunityForum></CommunityForum>} />
+        <Route path="/tournaments" element={<Tournaments />} />
+        <Route path="/merch" element={<Merch />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
