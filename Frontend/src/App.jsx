1577857f33fdd21f4e33822f4b27952492b7b849
jashrashne <jash.rashne@gmail.com>
2024-08-29 13:18:06 +0530

    community made mast

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage';
+import CommunityForum from './Pages/Community';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/community" element={<LandingPage />} />
+        <Route path="/community" element={<CommunityForum />} />
       </Routes>
     </Router>
   );
